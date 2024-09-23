06f6a4615d253248eca34065ec9f98b34048cfde
ES <eon@earncef.com>
2024-09-23 16:55:30 +0530

    implement from for primitive types

--- sqlc-core/src/lib.rs
+++ sqlc-core/src/lib.rs
@@ -3,3 +3,20 @@ use postgres::{Error, Row};
 pub trait FromPostgresRow: Sized {
     fn from_row(row: &Row) -> Result<Self, Error>;
 }
+
+macro_rules! from_primitive {
+    ($t:ty) => {
+        impl FromPostgresRow for $t {
+            fn from_row(row: &Row) -> Result<Self, Error> {
+                Ok(row.get::<&str, $t>("0"))
+            }
+        }
+    };
+}
+
+from_primitive!(bool);
+from_primitive!(String);
+from_primitive!(i16);
+from_primitive!(i32);
+from_primitive!(i64);
+from_primitive!(f64);
