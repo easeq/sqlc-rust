db000629adfc5d6b8daf57904760c24c37cde90b
ES <eon@earncef.com>
2024-10-29 16:37:33 +0530

    code cleanup

--- sqlc-core/src/batch-results.rs
+++ sqlc-core/src/batch-results.rs
@@ -1,32 +1,32 @@
 #[pin_project::pin_project]
 pub struct BatchResults<Callback, Fut, P> {
-    __pool: deadpool_postgres::Pool,
-    __stmt: tokio_postgres::Statement,
-    __index: usize,
+    pool: deadpool_postgres::Pool,
+    stmt: tokio_postgres::Statement,
+    index: usize,
 
-    __items: Vec<P>,
-    __total: usize,
+    items: Vec<P>,
+    total: usize,
 
-    __callback: Callback,
+    callback: Callback,
     #[pin]
-    __thunk: Option<Fut>,
+    thunk: Option<Fut>,
 }
 
 impl<Callback, Fut, P> BatchResults<Callback, Fut, P> {
     pub fn new(
-        __pool: deadpool_postgres::Pool,
-        __items: Vec<P>,
-        __stmt: tokio_postgres::Statement,
-        __callback: Callback,
+        pool: deadpool_postgres::Pool,
+        items: Vec<P>,
+        stmt: tokio_postgres::Statement,
+        callback: Callback,
     ) -> Self {
         Self {
-            __pool,
-            __total: __items.len(),
-            __items,
-            __stmt,
-            __callback,
-            __index: 0,
-            __thunk: None,
+            pool,
+            total: items.len(),
+            items,
+            stmt,
+            callback,
+            index: 0,
+            thunk: None,
         }
     }
 }
@@ -44,25 +44,25 @@ where
         cx: &mut std::task::Context<'_>,
     ) -> std::task::Poll<Option<Self::Item>> {
         let mut this = self.project();
-        if this.__index < this.__total {
-            let arg = this.__items[*this.__index].clone();
-            let stmt = this.__stmt.clone();
-            let pool = this.__pool.clone();
+        if this.index < this.total {
+            let arg = this.items[*this.index].clone();
+            let stmt = this.stmt.clone();
+            let pool = this.pool.clone();
 
-            if this.__thunk.is_none() {
-                this.__thunk.as_mut().set(Some((this.__callback)(
+            if this.thunk.is_none() {
+                this.thunk.as_mut().set(Some((this.callback)(
                     pool.clone(),
                     stmt.clone(),
                     arg.clone(),
                 )));
             }
 
-            let mut fut = this.__thunk.as_mut().as_pin_mut().unwrap();
+            let mut fut = this.thunk.as_mut().as_pin_mut().unwrap();
             match fut.as_mut().poll(cx) {
                 std::task::Poll::Ready(Err(e)) => std::task::Poll::Ready(Some(Err(e))),
                 std::task::Poll::Ready(Ok(res)) => {
-                    *this.__index += 1;
-                    this.__thunk.set(None);
+                    *this.index += 1;
+                    this.thunk.set(None);
                     std::task::Poll::Ready(Some(Ok(res)))
                 }
                 std::task::Poll::Pending => std::task::Poll::Pending,
